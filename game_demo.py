d9c4b243c975fc2f5d093f4fbe5f89733927b11f
diogo <d.cebola@campus.fct.unl.pt>
2021-06-09 22:38:02 +0100

    added heuristic

--- game_demo.py
+++ game_demo.py
@@ -4,8 +4,9 @@
 Created on Thu May 27 07:35:58 2021
 
 """
-import matplotlib.pyplot as plt
 from snake_game import SnakeGame
+from numpy.random import randint
+import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import Model
@@ -15,10 +16,13 @@ from tensorflow.keras.initializers import HeUniform
 from tensorflow.keras.optimizers import Adam
 from collections import deque
 import random
-import datetime
 import imageio
 from tqdm import tqdm
 import os
+from functools import partial
+import pickle
+
+tqdm = partial(tqdm, position=0, leave=True)
 
 
 def plot_board(name, _board, text):
@@ -30,14 +34,18 @@ def plot_board(name, _board, text):
     plt.close(fig)
 
 
-def plot_rewards(path, name, training, evaluation):
-    fig = plt.figure(figsize=(10, 10))
-    xticks = range(0, len(training), 1)
-    plt.plot(xticks, training, linestyle='--', label='Training', color='cornflowerblue')
-    plt.plot(xticks, evaluation, linestyle='-', label='Evaluation', color='orange')
-    plt.legend()
-    plt.xlabel("Epochs")
-    plt.ylabel("Reward")
+def plot_apples_steps(path, name, rewards, apples, _steps):
+    fig, axs = plt.subplots(1, 3, figsize=(9, 3))
+    axs[0].set_xlabel("Games")
+    axs[0].set_ylabel("Apples")
+    axs[0].plot(range(0, len(apples), 1), apples, linestyle='-', label='Apples', color='red')
+    axs[1].set_xlabel("Games")
+    axs[1].set_ylabel("Steps")
+    axs[1].plot(range(0, len(_steps), 1), _steps, linestyle='-', label='Steps', color='cornflowerblue')
+    axs[2].set_xlabel("Games")
+    axs[2].set_ylabel("Total Reward")
+    axs[2].plot(range(0, len(rewards), 1), rewards, linestyle='-', label='Steps', color='orange')
+
     plt.tight_layout()
     plt.savefig(path + "/" + name)
     plt.close(fig)
@@ -52,7 +60,7 @@ def CNNModel(inputs):
     layer = MaxPooling2D(pool_size=(2, 2), padding='same')(layer)
     layer = Conv2D(128, kernel_size=(3, 3), activation="relu", padding='same')(layer)
     layer = Conv2D(128, kernel_size=(3, 3), activation="relu", padding='same')(layer)
-    layer = Flatten(name='features')(layer)
+    layer = Flatten()(layer)
     layer = Dense(128, activation='relu')(layer)
     layer = BatchNormalization()(layer)
     layer = Dropout(0.5)(layer)
@@ -65,13 +73,13 @@ def snakeModel(state_shape, action_shape, optimizer, loss, cnn=False):
     layer = inputs
     if cnn:
         layer = CNNModel(inputs)
-    layer = Flatten(name='features')(layer)
+    else:
+        layer = Flatten()(layer)
     layer = Dense(64, input_shape=state_shape, activation='relu', kernel_initializer=init)(layer)
     layer = Dense(32, activation='relu', kernel_initializer=init)(layer)
     outputs = Dense(action_shape, activation='softmax', kernel_initializer=init)(layer)
     model = Model(inputs=inputs, outputs=outputs)
     model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
-    model.summary()
     return model
 
 
@@ -79,7 +87,6 @@ def decide(agent, state, _epsilon):
     if np.random.rand() <= _epsilon:
         return np.random.choice(POLICY, 1)
     predicted = agent.predict(state.reshape(BATCHED_SHAPE)).flatten()
-    # print("PREDICTED:", np.argmax(predicted) - 1)
     return np.argmax(predicted) - 1
 
 
@@ -110,6 +117,274 @@ def generateGame(scenario, width=30, height=30, border=1):
                      grass_growth=scenario.get('GRASS_GROWTH'))
 
 
+def check_collisions(next_pos, tails):
+    if (next_pos[0] == -1 or next_pos[0] == 33
+            or next_pos[1] == -1 or next_pos[1] == 33
+            or next_pos in tails):
+        return True
+    else:
+        return False
+
+
+def heuristic(_, apples, head, tails, direction, _type):
+    act = 0
+    if _type == RANDOM_PATTERN:
+        return np.random.choice(POLICY, 1)  # Random move
+    else:
+        # Find Closest Apple
+        closest_apple = apples[0]
+        min_row = 32
+        min_col = 32
+
+        for apple in apples:
+            d_row = apple[0] - head[0]
+            d_col = apple[1] - head[1]
+            if d_row <= min_row and d_col <= min_col:
+                min_row = d_row
+                min_col = d_col
+                closest_apple = apple
+
+        # Find Next Move
+        d_row = closest_apple[0] - head[0]
+        d_col = closest_apple[1] - head[1]
+
+        if _type == L_PATTERN:  # L pattern heuristic
+            if abs(d_row) > 0:
+                if d_row < 0:
+                    ddir = 0  # -x
+                else:
+                    ddir = 2  # +x
+            else:
+                if d_col < 0:
+                    ddir = 3  # -y
+                else:
+                    ddir = 1  # +y
+        else:
+            if abs(d_row) > abs(d_col):  # Diagonal pattern heuristic
+                if d_row < 0:
+                    ddir = 0  # -x
+                else:
+                    ddir = 2  # +x
+            else:
+                if d_col < 0:
+                    ddir = 3  # -y
+                else:
+                    ddir = 1  # +y
+
+        if ddir == 0 and direction == 0:
+            act = 0
+        if ddir == 1 and direction == 0:
+            act = 1
+        if ddir == 2 and direction == 0:
+            act = 1
+        if ddir == 3 and direction == 0:
+            act = -1
+
+        if ddir == 0 and direction == 1:
+            act = -1
+        if ddir == 1 and direction == 1:
+            act = 0
+        if ddir == 2 and direction == 1:
+            act = 1
+        if ddir == 3 and direction == 1:
+            act = 1
+
+        if ddir == 0 and direction == 2:
+            act = 1
+        if ddir == 1 and direction == 2:
+            act = -1
+        if ddir == 2 and direction == 2:
+            act = 0
+        if ddir == 3 and direction == 2:
+            act = 1
+
+        if ddir == 0 and direction == 3:
+            act = 1
+        if ddir == 1 and direction == 3:
+            act = 1
+        if ddir == 2 and direction == 3:
+            act = -1
+        if ddir == 3 and direction == 3:
+            act = 0
+
+        # Check If Valid Move
+        _done = False
+        looping = False
+
+        while not _done:
+            if direction == 0:
+                if act == -1:
+                    next_pos = (head[0], head[1] - 1)
+                    if check_collisions(next_pos, tails):
+                        act = 0
+                    else:
+                        _done = True
+                if looping:
+                    _done = True
+                if act == 0:
+                    next_pos = (head[0] - 1, head[1])
+                    if check_collisions(next_pos, tails):
+                        act = 1
+                    else:
+                        _done = True
+                if act == 1:
+                    next_pos = (head[0], head[1] + 1)
+                    if check_collisions(next_pos, tails):
+                        act = -1
+                        looping = True
+                    else:
+                        _done = True
+            if direction == 1:
+                if act == -1:
+                    next_pos = (head[0] - 1, head[1])
+                    if check_collisions(next_pos, tails):
+                        act = 0
+                    else:
+                        _done = True
+                if looping:
+                    _done = True
+                if act == 0:
+                    next_pos = (head[0], head[1] + 1)
+                    if check_collisions(next_pos, tails):
+                        act = 1
+                    else:
+                        _done = True
+                if act == 1:
+                    next_pos = (head[0] + 1, head[1])
+                    if check_collisions(next_pos, tails):
+                        act = -1
+                        looping = True
+                    else:
+                        _done = True
+            if direction == 2:
+                if act == -1:
+                    next_pos = (head[0], head[1] + 1)
+                    if check_collisions(next_pos, tails):
+                        act = 0
+                    else:
+                        _done = True
+                if looping:
+                    _done = True
+                if act == 0:
+                    next_pos = (head[0] + 1, head[1])
+                    if check_collisions(next_pos, tails):
+                        act = 1
+                    else:
+                        _done = True
+                if act == 1:
+                    next_pos = (head[0], head[1] - 1)
+                    if check_collisions(next_pos, tails):
+                        act = -1
+                        looping = True
+                    else:
+                        _done = True
+            if direction == 3:
+                if act == -1:
+                    next_pos = (head[0] + 1, head[1])
+                    if check_collisions(next_pos, tails):
+                        act = 0
+                    else:
+                        _done = True
+                if looping:
+                    _done = True
+                if act == 0:
+                    next_pos = (head[0], head[1] - 1)
+                    if check_collisions(next_pos, tails):
+                        act = 1
+                    else:
+                        _done = True
+                if act == 1:
+                    next_pos = (head[0] - 1, head[1])
+                    if check_collisions(next_pos, tails):
+                        act = -1
+                        looping = True
+                    else:
+                        _done = True
+        return act
+
+
+def generateMemory(length, scenarios):
+    mem = deque(maxlen=length)
+    memory_loaded = False
+    if os.path.isfile(os.getcwd() + "/" + "memory.pkl"):
+        with open(os.getcwd() + "/" + "memory.pkl") as f:
+            try:
+                print("Loaded memory.")
+                memory_loaded = True
+                mem = pickle.load(f)
+            except Exception:
+                pass
+    if memory_loaded is False:
+        print("Generating memory with examples.")
+        mem_apples = []
+        mem_steps = []
+        mem_rewards = []
+        not_filled = True
+        example_count = 0
+        games = 0
+        heuristic_type = RANDOM_PATTERN
+        interesting_examples = 0
+        normal_examples = 0
+        while not_filled:
+            _game = generateGame(scenarios[games % (len(scenarios))])
+            _board, _reward, _, _info = _game.reset()
+            _done = False
+            step = 0
+            mem_apples_eaten = 0
+            cumulative_rewards = 0
+            while not _done:
+                score, apples, head, tails, direction = _game.get_state()
+                _action = heuristic(score, apples, head, tails, direction, heuristic_type)
+                _next_board, _reward, _done, _info = _game.step(_action)
+                cumulative_rewards += _reward
+
+                if _reward >= 1:
+                    mem_apples_eaten += 1
+
+                if _done or _reward >= 1:
+                    example_count += 1
+                    mem.append([_board, _action, _reward, _next_board, _done])
+                    interesting_examples += 1
+                else:
+                    if np.random.rand() <= 0.05:
+                        example_count += 1
+                        normal_examples += 1
+                        mem.append([_board, _action, _reward, _next_board, _done])
+
+                """
+                file_name = PATH + "/" + str(games) + "_" + str(steps) + ".png"
+                plot_board(file_name, board, str(games) + ", " + str(steps))
+                memfill_gif.append_data(imageio.imread(file_name))
+                memfill_gif.append_data(imageio.imread(file_name))
+                os.remove(file_name)
+                """
+
+                _board = _next_board
+                step = step + 1
+                if example_count == round(length / 5):
+                    heuristic_type = L_PATTERN
+                elif example_count == round(3 * length / 5):
+                    heuristic_type = DIAGONAL_PATTERN
+                if example_count == length:
+                    not_filled = False
+                if step == MAX_STEPS:
+                    _done = True
+            mem_rewards.append(cumulative_rewards)
+            mem_apples.append(mem_apples_eaten)
+            mem_steps.append(step)
+            games += 1
+        pickle.dump(mem, open(os.getcwd() + "/" + "memory.pkl", "wb"))
+        plot_apples_steps(PATH, 'Heuristic.png', mem_rewards, mem_apples, mem_steps)
+        print("Memory saved to pickle file. (" +
+              str(interesting_examples * 100 / length) + "% interesting, " +
+              str(normal_examples * 100 / length) + "% normal)")
+    return mem
+
+
+RANDOM_SEED = 5
+tf.random.set_seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
+
 RANDOM_SEED = 5
 tf.random.set_seed(RANDOM_SEED)
 np.random.seed(RANDOM_SEED)
@@ -125,43 +400,51 @@ POLICY = [-1, 0, 1]
 
 BATCH_SIZE = 64 * 2
 MEMORY_LENGTH = 50000
-EPOCHS = 150
-MIN_EPOCHS = 500
-MAX_STEPS = 300
-TRAIN_STEPS = 4
-TARGET_DELAY = 50
+MIN_EPOCHS = 1000
+EPOCHS = 100
+MAX_STEPS = 500
 
 # TODO: experiment with different values for different heuristics
 MAX_EPSILON = 1
 MIN_EPSILON = 0.01
 DECAY_RATE = 0.01
+
 DISCOUNT = 0.618
 LEARNING_RATE = 0.001
+TARGET_DELAY = 100
+TRAIN_STEPS = 4
+
+
+RANDOM_PATTERN = 0
+L_PATTERN = 1
+DIAGONAL_PATTERN = 2
 
 if __name__ == '__main__':
-    train_scenarios = [{'FOOD_AMOUNT': 5, 'MAX_GRASS': 0.05, 'GRASS_GROWTH': 0.0001},
-                       {'FOOD_AMOUNT': 15, 'MAX_GRASS': 0, 'GRASS_GROWTH': 0},
-                       {'FOOD_AMOUNT': 5, 'MAX_GRASS': 0, 'GRASS_GROWTH': 0}]
+    os.makedirs(PATH, exist_ok=True)
+    #training_gif = imageio.get_writer(PATH + "/" + 'training.gif', mode='I')
+
+    train_scenarios = [{'FOOD_AMOUNT': 1, 'MAX_GRASS': 0.2, 'GRASS_GROWTH': 0.0001},
+                       {'FOOD_AMOUNT': 5, 'MAX_GRASS': 0, 'GRASS_GROWTH': 0},
+                       {'FOOD_AMOUNT': 1, 'MAX_GRASS': 0, 'GRASS_GROWTH': 0}]
 
     eval_scenario = {'FOOD_AMOUNT': 1, 'MAX_GRASS': 0, 'GRASS_GROWTH': 0}
 
-    training_gif = imageio.get_writer(PATH + "/" + 'training.gif', mode='I')
-    final_gif = imageio.get_writer(PATH + "/" + 'play.gif', mode='I')
-    snake = snakeModel(BOARD_SHAPE, len(POLICY), Adam(lr=LEARNING_RATE), Huber())
-    target_snake = snakeModel(BOARD_SHAPE, len(POLICY), Adam(lr=LEARNING_RATE), Huber())
+    memory = generateMemory(MEMORY_LENGTH, [{'FOOD_AMOUNT': 1, 'MAX_GRASS': 0, 'GRASS_GROWTH': 0}])
+
+    snake = snakeModel(BOARD_SHAPE, len(POLICY), Adam(lr=LEARNING_RATE), Huber(), cnn=True)
+    target_snake = snakeModel(BOARD_SHAPE, len(POLICY), Adam(lr=LEARNING_RATE), Huber(), cnn=True)
     target_snake.set_weights(snake.get_weights())
-    memory = deque(maxlen=MEMORY_LENGTH)
-    # TODO: Fill memory (Pickle?)
 
     epsilon = MAX_EPSILON
     game = None
     steps = 0
+    train_apples = []
+    train_steps = []
     train_rewards = []
-    eval_rewards = []
 
     train_border = TRAIN_BORDER
-    train_width = 32 - 2*TRAIN_BORDER
-    train_height = 32 - 2*TRAIN_BORDER
+    train_width = 32 - 2 * TRAIN_BORDER
+    train_height = 32 - 2 * TRAIN_BORDER
     border_inc_interval = round(EPOCHS / (train_border - 1))
 
     for epoch in tqdm(range(EPOCHS), desc="Training"):
@@ -170,57 +453,48 @@ if __name__ == '__main__':
             train_height += 2
             train_width += 2
         game = generateGame(train_scenarios[epoch % len(train_scenarios)], train_width, train_height, train_border)
+        # game = generateGame(train_scenarios[epoch % len(train_scenarios)])
+        # game = generateGame(train_scenarios[epoch % 2], train_width, train_height, train_border)
+        # game = generateGame(eval_scenario, train_width, train_height, train_border)
+
         board, reward, _, info = game.reset()
-        total_reward = 0
+        total_apples = 0
+        total_steps = 0
+        total_rewards = 0
         done = False
         while not done:
+            """
             if not done:
                 file_name = PATH + "/" + str(epoch) + "_" + str(steps) + ".png"
                 plot_board(file_name, board, str(epoch) + ", " + str(steps))
                 training_gif.append_data(imageio.imread(file_name))
                 os.remove(file_name)
+            """
             steps += 1
+            total_steps += 1
             action = decide(snake, board, epsilon)
             next_board, reward, done, info = game.step(action)
-
             memory.append([board, action, reward, next_board, done])
+            total_rewards += reward
             if len(memory) >= MIN_EPOCHS and (steps % TRAIN_STEPS == 0 or done):
                 train(memory, snake, target_snake)
-
+            if reward >= 1:
+                total_apples += 1
             board = next_board
-            total_reward += reward
             if done:
+                """
                 file_name = PATH + "/" + str(epoch) + "_" + str(steps) + ".png"
                 plot_board(file_name, board, str(epoch) + ", " + str(steps))
                 training_gif.append_data(imageio.imread(file_name))
                 training_gif.append_data(imageio.imread(file_name))
                 os.remove(file_name)
-                train_rewards.append(total_reward)
+                """
+                train_apples.append(total_apples)
+                train_steps.append(total_steps)
+                train_rewards.append(total_rewards)
+                total_steps = 0
                 if steps >= TARGET_DELAY:
                     target_snake.set_weights(snake.get_weights())
                     steps = 0
-        total_reward = 0
-        done = False
-        eval_game = generateGame(eval_scenario)
-        board, reward, _, info = eval_game.reset()
-        if epoch % 20:
-            while not done:
-                if epoch + 1 == EPOCHS:
-                    file_name = PATH + "/" + str(epoch) + "_" + str(steps) + ".png"
-                    plot_board(file_name, board, str(epoch) + ", " + str(steps))
-                    final_gif.append_data(imageio.imread(file_name))
-                    os.remove(file_name)
-                action = decide(snake, board, epsilon)
-                board, reward, done, info = eval_game.step(action)
-                total_reward += reward
-                if done:
-                    # print('Eval Reward: total = {}, final = {}'.format(total_reward, reward))
-                    if epoch + 1 == EPOCHS:
-                        file_name = PATH + "/" + str(epoch) + "_" + str(steps) + ".png"
-                        plot_board(file_name, board, str(epoch) + ", " + str(steps))
-                        final_gif.append_data(imageio.imread(file_name))
-                        final_gif.append_data(imageio.imread(file_name))
-                        os.remove(file_name)
-                    eval_rewards.append(total_reward)
         epsilon = MIN_EPSILON + (MAX_EPSILON - MIN_EPSILON) * np.exp(-DECAY_RATE * epoch)
-    plot_rewards(PATH, 'Rewards.png', train_rewards, eval_rewards)
+    plot_apples_steps(PATH, 'Train.png', train_rewards, train_apples, train_steps)
